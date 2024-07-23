f55677cdacd1126ef179b34b1052f856784f6352
ahmed.laouar <ahmed.laouar@univ-artois.fr>
2024-07-23 12:01:51 +0200

    Adding acceptance for permission support vs prohibition supports.

--- compute-accepted.py
+++ compute-accepted.py
@@ -5,8 +5,16 @@ from urllib.parse import urlparse
 def strip_prefix(uri):
     return uri.split('#')[-1]
 
-def compute_supports(graph, subject, action, object):
-    supports_query_path = "queries.sparql/supports_query.sparql"
+def compute_supports(graph, subject, action, object, accessType=0):
+    # by default, support of a permission is computed when accessType=0
+    # for a support of a prohibition set accessType = 1
+    if accessType == 0:
+        supports_query_path = "queries.sparql/supports_query.sparql"
+    elif accessType == 1:
+        supports_query_path = "queries.sparql/prohibition_supports_query.sparql"
+    else:
+        print("Please enter a valid accessType.")
+
     with open(supports_query_path, 'r') as file:
         
         query_template = file.read()
@@ -74,8 +82,8 @@ def check_dominance(graph, subset1, subset2):
             return False
     return True
 
-def check_acceptance(graph, subject, action, object):
-    supports = compute_supports(graph, subject, action, object)
+def check_acceptance_vs_all_conflicts(graph, subject, action, object):
+    supports = compute_supports(graph, subject, action, object, 0)
     conflicts = compute_conflicts(graph)
     stripped_conflicts = []
     for conflict in conflicts:
@@ -85,8 +93,7 @@ def check_acceptance(graph, subject, action, object):
     for support in supports:
         stripped_supports.append(tuple(strip_prefix(str(uri)) for uri in support))
     stripped_supports = list(set(stripped_supports))
-    #print(stripped_supports)
-    #print(stripped_conflicts)
+    
     accepted = True
     for conflict in stripped_conflicts:
         conflict_supported = False
@@ -98,12 +105,37 @@ def check_acceptance(graph, subject, action, object):
             return False
     return accepted
 
+def check_acceptance(graph, subject, action, object):
+    permission_supports = compute_supports(graph, subject, action, object, 0)
+    prohibition_supports = compute_supports(graph, subject, action, object, 1)
+    
+    stripped_prohibition_supports = []
+    for proh_support in prohibition_supports:
+        stripped_prohibition_supports.append(tuple(strip_prefix(str(uri)) for uri in proh_support))
+    stripped_prohibition_supports = list(set(stripped_prohibition_supports))
+    stripped_permission_supports = []
+    for perm_support in permission_supports:
+        stripped_permission_supports.append(tuple(strip_prefix(str(uri)) for uri in perm_support))
+    stripped_permission_supports = list(set(stripped_permission_supports))
+    print(stripped_permission_supports)
+    print(stripped_prohibition_supports)
+    accepted = True
+    for proh_support in stripped_prohibition_supports:
+        conflict_supported = False
+        for perm_support in stripped_permission_supports:
+            if check_dominance(graph,perm_support, proh_support):
+                conflict_supported = True
+                break
+        if not conflict_supported:
+            return False
+    return accepted
+
 # Load the ontology
 graph = Graph()
 graph.parse("ontology/orbac-STARWARS.owl", format="xml")
 
-#subject, object, action = "Bob", "report1", "edit"
-subject, object, action = 'researcher4', 'dataset5', 'select'
+subject, object, action = "Bob", "report1", "edit"
+
 
 
 if check_acceptance(graph, subject, action, object):
@@ -111,12 +143,13 @@ if check_acceptance(graph, subject, action, object):
 else:
     print(f"The permission for {subject} to perform the action {action} on {object} is denied")
 
-
-#if check_consistency(graph):
-#    print("The instance is consistent")
-#else:
-#    print("The instance is inconsistent")
 """
+subject, object, action = 'researcher4', 'dataset5', 'select'
+if check_consistency(graph):
+    print("The instance is consistent")
+else:
+    print("The instance is inconsistent")
+
 supports = compute_supports(graph, subject, action, object)
 for support in supports:
     stripped_support = tuple(strip_prefix(str(uri)) for uri in support)
