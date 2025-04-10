bd70e0d1b727fa9286f0ac9c7cff177f01730d48
ahmedlaouar <ahmedlaouar.al@gmail.com>
2025-04-10 06:16:39 +0200

    add functions and results of LLM exps

---
+++ web-demo/Policy.py
@@ -0,0 +1,72 @@
+from logzero import logger
+from rdflib import OWL, RDF, Graph
+
+class Policy:
+    def __init__(self, instance_file_path):
+        logger.debug('Loading Policy.')
+        self.graph = Graph()
+        # Paths to the base ontology and the instance file
+        base_ontology_path = "ontology/orbac.owl"        
+        # Parse the base ontology into the graph
+        self.graph.parse(base_ontology_path, format="xml")
+        # Parse the instance file into the same graph
+        self.graph.parse(instance_file_path, format="xml")
+        for s, p, o in self.graph.triples((None, RDF.type, OWL.Ontology)):
+            base_uri = s
+            #break  # Assuming there is only one owl:Ontology
+        if base_uri:
+            self.example_uri = base_uri
+        else:
+            logger.debug("Base URI not found in the graph.")
+        if self.example_uri[-1] != "#":
+            self.example_uri += "#"
+        logger.debug('Ready.')
+
+    def strip_prefix(self, uri):
+        return uri.split('#')[-1]
+    
+    def get_concrete_concepts(self):
+        query = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX : <https://raw.githubusercontent.com/ahmedlaouar/orbac.owl/refs/heads/main/ontology/orbac.owl#> # Adjust the base URI as necessary
+
+                SELECT ?s ?alpha ?o {{
+                    ?relation rdf:type :Define .
+                    ?relation :definesSubject ?s .
+                    ?relation :definesAction ?alpha .
+                    ?relation :definesObject ?o .
+                }}"""        
+        results = self.graph.query(query)
+        return results
+    
+    def select_combination(self):
+        # randomly get a subject, action and object from the used example
+        define_rules = self.get_concrete_concepts()
+        define_rules_data = [
+            {"Subject": self.strip_prefix(row[0]), 
+            "Action": self.strip_prefix(row[1]), 
+            "Object": self.strip_prefix(row[2])}
+            for row in define_rules
+        ]
+
+        # Remove duplicates
+        combinations = {tuple(d.values()) for d in define_rules_data}
+        combinations = [dict(zip(["Subject", "Action", "Object"], comb)) for comb in combinations]        
+        random_row = combinations.sample(n=1)
+        subject, action, obj = random_row["Subject"].values[0], random_row["Action"].values[0], random_row["Object"].values[0]
+        return subject, action, obj
+    
+    def get_combinations(self):
+        """retruens all the combinations of subject, action and object without duplicates"""
+        # Convert to list of dictionaries and apply strip_prefix
+        define_rules = self.get_concrete_concepts()
+        define_rules_data = [
+            {"Subject": self.strip_prefix(row[0]), 
+            "Action": self.strip_prefix(row[1]), 
+            "Object": self.strip_prefix(row[2])}
+            for row in define_rules
+        ]
+
+        # Remove duplicates
+        combinations = {tuple(d.values()) for d in define_rules_data}
+        combinations = [dict(zip(["Subject", "Action", "Object"], comb)) for comb in combinations]
+        return combinations
